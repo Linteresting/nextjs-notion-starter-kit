77f403c9bb5a6d4ebeeae35ed65a9be6234eca03
Linteresting <37887557+Linteresting@users.noreply.github.com>
2023-11-20 13:27:59 +0800

    install Clarity

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,6 +82,13 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
+      <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jt9jqhpl68");
+      </script>
     </Head>
   )
 }
