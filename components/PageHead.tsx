8bb2c3b9107055271d67923acfa284fff493320a
Linteresting <37887557+Linteresting@users.noreply.github.com>
2023-11-20 13:37:17 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -84,9 +84,14 @@ export const PageHead: React.FC<
       <title>{title}</title>
       <script type="text/javascript">
           (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              c[a]=c[a] || function() {
+                (c[a].q=c[a].q||[]).push(arguments);
+              };
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "jt9jqhpl68");
       </script>
     </Head>
